c7d542378f29140322adbce159c6494e459410d3
Teusner <quentin.brateau@ensta-bretagne.org>
2023-02-21 10:39:26 +0100

    Update

--- scripts/001-1s_1m.py
+++ scripts/001-1s_1m.py
@@ -70,7 +70,6 @@ class Mission(Node):
     def get_result_callback(self, future):
         result = future.result().result
         self.get_logger().info('Final depth: {0}'.format(result.sequence))
-        rclpy.shutdown()
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
@@ -81,12 +80,12 @@ class Mission(Node):
             if self.state == State.IDLE and self.p_min <= self.pressure:
                 # Call 1 m action
                 self.get_logger().info("Calling Action 1 m")
-                self.send_goal(1)
+                self.send_goal(1.)
                 self.state = State.ACTION1M
             elif self.state == State.REACH1M and time.time() - self.t0 > self.duration_1m:
                 # Call 0 m action
                 self.get_logger().info("Calling Action 0 m")
-                self.send_goal(0)
+                self.send_goal(0.)
                 self.state = State.ACTION0M
         else:
             self.get_logger().info("State IDLE")
