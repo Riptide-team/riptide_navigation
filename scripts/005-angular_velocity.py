310920ff42edd2562d56a144a346d334748e2b15
Teusner <quentin.brateau@ensta-bretagne.org>
2023-05-31 11:05:59 +0200

    Update

--- scripts/005-angular_velocity.py
+++ scripts/005-angular_velocity.py
@@ -24,9 +24,9 @@ class Mission(Node):
         self.load_riptide_controller()
 
         # Twist publisher
-        self.get_logger().info("Waiting for angular velocity on `~/riptide_controller/cmd_vel`")
+        self.get_logger().info("Publishing Twist on `/riptide_1/riptide_controller/cmd_vel`")
         self.publisher_ = self.create_publisher(Twist, '/riptide_1/riptide_controller/cmd_vel', 10)
-        timer_period = 0.5  # seconds
+        timer_period = 1.  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def load_riptide_controller(self):
